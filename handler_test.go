20f1401857748bda1bf256b82d63ab4226479e2b
daffanugraha21 <dnr.project12@gmail.com>
2026-01-14 09:38:03 +0700

    add test for HTTP handler

---
+++ handler_test.go
@@ -0,0 +1,22 @@
+package belajar_golang_web
+import (
+	"testing"
+	"net/http"
+	"fmt"
+)
+
+func TestHandler(t *testing.T){
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request){
+		//logic web
+		fmt.Fprint(writer, "Hello World")
+	}
+
+	server := http.Server{
+		Addr: "localhost:8080",
+		Handler: handler,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
