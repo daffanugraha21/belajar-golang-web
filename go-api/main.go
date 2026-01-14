0451b120da34ddc669a349515510a4a424ba3cc0
daffanugraha21 <dnr.project12@gmail.com>
2026-01-14 10:55:35 +0700

    refactor main function to use route registration from routes package

--- go-api/main.go
+++ go-api/main.go
@@ -1,33 +1,13 @@
 package main
 
 import (
-	"net/http"
 	"github.com/gin-gonic/gin"
+	"go-api/routes"
 )
 
-func main () {
+func main(){
 	r := gin.Default()
-
-	r .GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-
-	r.POST("/hello", func(c *gin.Context) {
-		var body struct {
-			Name string `jsone:"name"`
-		}
-
-		if err := c.ShouldBindJSON(&body); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"message": "invalid request",
-		})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{
-            "message": "hello " + body.Name,
-  	})
-	})
+	routes.RegisterRoutes(r)
 	r.Run(":8080")
 }
+
